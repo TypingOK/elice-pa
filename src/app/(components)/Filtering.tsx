607b96c3211bd4d0cb142591872e160e981c12e8
TypingOK <wjdwls717@gmail.com>
2024-02-23 14:08:25 +0900

    Feat: 필터 선택, input 태그에 입력시  주소창에 반영 되도록 하기

--- src/app/(components)/Filtering.tsx
+++ src/app/(components)/Filtering.tsx
@@ -1,20 +1,70 @@
 "use client";
 import { SubmitHandler, useForm } from "react-hook-form";
 import SearchArea from "./SearchArea";
-
+import {
+  category,
+  courseType,
+  format,
+  level,
+  price,
+  programmingLanguage,
+} from "@/constants/filtering";
+import { Badge } from "@/components/ui/badge";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export interface FormData {
   [key: string]: string;
 }
 
+function isNumeric(str: string) {
+  return /^[0-9]+$/.test(str);
+}
+
 const FilteringWrapper = () => {
-  const { setValue, register, handleSubmit, getValues } = useForm<FormData>();
+  const { setValue, register, handleSubmit, getValues, watch } =
+    useForm<FormData>();
+  const searchParams = useSearchParams();
+  console.log(searchParams.getAll("courseType"));
 
   const onSubmit: SubmitHandler<FormData> = (data: FormData) => {
     console.log(data);
+    let path = "";
+    Object.keys(data).forEach((e) => {
+      if (data[e] && data[e] !== "") {
+        if (isNumeric(e)) {
+          const index = parseInt(e);
+          if (1 <= index && index <= 3) {
+            path += `&${courseType.pathType}=${index}`;
+          } else if (4 <= index && index <= 5) {
+            path += `&${format.pathType}=${index}`;
+          } else if (6 <= index && index <= 10) {
+            path += `&${category.pathType}=${index}`;
+          } else if (11 <= index && index <= 15) {
+            path += `&${level.pathType}=${index}`;
+          } else if (16 <= index && index <= 28) {
+            path += `&${programmingLanguage.pathType}=${index}`;
+          } else if (29 <= index && index <= 32) {
+            path += `&${price.pathType}=${index}`;
+          }
+        }
+      }
+      if (e === "keyword" && data[e] !== "") {
+        path += `&keyword=${data[e]}`;
+      }
+    });
+    if (window) {
+      window.history.pushState({}, "", `?${path}`);
+    }
   };
 
-  
+  const handleFilter = (name: string, label: number) => {
+    const labelValue = getValues(label.toString());
+    if (labelValue === "" || labelValue === undefined) {
+      setValue(label.toString(), name);
+    } else {
+      setValue(label.toString(), "");
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -25,7 +75,154 @@ const FilteringWrapper = () => {
         onSubmit={onSubmit}
         setValue={setValue}
       />
-      
+      <table className="w-full">
+        <tbody className="border">
+          <tr className="border">
+            <td className="min-w-24 py-[0.875rem] px-4 text-sm border-r bg-stone-100">
+              유형
+            </td>
+            <td className="p-2">
+              {Object.keys(courseType.filter).map((e) => (
+                <Badge
+                  key={courseType.filter[e].index}
+                  onClick={() => {
+                    handleFilter(e, courseType.filter[e].index);
+                  }}
+                  variant={
+                    watch(courseType.filter[e].index.toString()) === e
+                      ? "default"
+                      : "outline"
+                  }
+                  className={`min-w-[1.875rem] mx-2 my-1 `}
+                  {...register(courseType.filter[e].index.toString())}
+                >
+                  {e}
+                </Badge>
+              ))}
+            </td>
+          </tr>
+          <tr className="border">
+            <td className="min-w-24 py-[0.875rem] px-4 text-sm border-r bg-stone-100">
+              진행방식
+            </td>
+            <td className="p-2">
+              {Object.keys(format.filter).map((e) => (
+                <Badge
+                  key={format.filter[e].index}
+                  onClick={() => {
+                    handleFilter(e, format.filter[e].index);
+                  }}
+                  variant={
+                    watch(format.filter[e].index.toString()) === e
+                      ? "default"
+                      : "outline"
+                  }
+                  className={`min-w-[1.875rem] mx-2 my-1`}
+                  {...register(format.filter[e].index.toString())}
+                >
+                  {e}
+                </Badge>
+              ))}
+            </td>
+          </tr>
+          <tr className="border">
+            <td className="min-w-24 py-[0.875rem] px-4 text-sm border-r bg-stone-100">
+              분야
+            </td>
+            <td className="p-2">
+              {Object.keys(category.filter).map((e) => (
+                <Badge
+                  key={category.filter[e].index}
+                  onClick={() => {
+                    handleFilter(e, category.filter[e].index);
+                  }}
+                  variant={
+                    watch(category.filter[e].index.toString()) === e
+                      ? "default"
+                      : "outline"
+                  }
+                  className={`min-w-[1.875rem] mx-2 my-1`}
+                  {...register(category.filter[e].index.toString())}
+                >
+                  {e}
+                </Badge>
+              ))}
+            </td>
+          </tr>
+          <tr className="border">
+            <td className="min-w-24 py-[0.875rem] px-4 text-sm border-r bg-stone-100">
+              난이도
+            </td>
+            <td className="p-2">
+              {Object.keys(level.filter).map((e) => (
+                <Badge
+                  key={level.filter[e].index}
+                  onClick={() => {
+                    handleFilter(e, level.filter[e].index);
+                  }}
+                  variant={
+                    watch(level.filter[e].index.toString()) === e
+                      ? "default"
+                      : "outline"
+                  }
+                  className={`min-w-[1.875rem] mx-2 my-1`}
+                  {...register(level.filter[e].index.toString())}
+                >
+                  {e}
+                </Badge>
+              ))}
+            </td>
+          </tr>
+          <tr className="border">
+            <td className="min-w-24 py-[0.875rem] px-4 text-sm border-r bg-stone-100">
+              언어
+            </td>
+            <td className="p-2">
+              {Object.keys(programmingLanguage.filter).map((e) => (
+                <Badge
+                  key={programmingLanguage.filter[e].index}
+                  onClick={() => {
+                    handleFilter(e, programmingLanguage.filter[e].index);
+                  }}
+                  variant={
+                    watch(programmingLanguage.filter[e].index.toString()) === e
+                      ? "default"
+                      : "outline"
+                  }
+                  className={`min-w-[1.875rem] mx-2 my-1`}
+                  {...register(programmingLanguage.filter[e].index.toString())}
+                >
+                  {e}
+                </Badge>
+              ))}
+            </td>
+          </tr>
+          <tr className="border">
+            <td className="min-w-24 py-[0.875rem] px-4 text-sm border-r bg-stone-100">
+              가격
+            </td>
+            <td className="p-2">
+              {Object.keys(price.filter).map((e) => (
+                <Badge
+                  key={price.filter[e].index}
+                  onClick={() => {
+                    handleFilter(e, price.filter[e].index);
+                  }}
+                  variant={
+                    watch(price.filter[e].index.toString()) === e
+                      ? "default"
+                      : "outline"
+                  }
+                  className={`min-w-[1.875rem] mx-2 my-1`}
+                  {...register(price.filter[e].index.toString())}
+                >
+                  {e}
+                </Badge>
+              ))}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </form>
   );
 };
--- src/constants/filtering.ts
+++ src/constants/filtering.ts
@@ -1,61 +1,85 @@
-export const courseType: {
-  [key: string]: number;
-} = {
-  과목: 1,
-  챌린지: 2,
-  테스트: 3,
+export interface CourseFilter {
+  filterType: "course_type" | "tag_id" | "enroll_type";
+  pathType:
+    | "courseType"
+    | "format"
+    | "category"
+    | "level"
+    | "programmingLanguage"
+    | "price";
+  filter: { [key: string]: { index: number; data: number[] } };
+}
+
+export const courseType: CourseFilter = {
+  filter: {
+    과목: { index: 1, data: [0, 2] },
+    챌린지: { index: 2, data: [1] },
+    테스트: { index: 3, data: [3] },
+  },
+  pathType: "courseType",
+  filterType: "course_type",
 };
 
-export const format: {
-  [key: string]: number;
-} = {
-  "자유 선택형": 4,
-  "순차 완료형": 5,
+export const format: CourseFilter = {
+  filter: {
+    "자유 선택형": { index: 4, data: [0] },
+    "순차 완료형": { index: 5, data: [2] },
+  },
+  filterType: "course_type",
+  pathType: "format",
 };
 
-export const category: {
-  [key: string]: number;
-} = {
-  "프로그래밍 기초": 6,
-  "데이터 분석": 7,
-  웹: 8,
-  인공지능: 9,
-  알고리즘: 10,
+export const category: CourseFilter = {
+  filter: {
+    "프로그래밍 기초": { index: 6, data: [12] },
+    "데이터 분석": { index: 7, data: [13] },
+    웹: { index: 8, data: [14] },
+    인공지능: { index: 9, data: [22] },
+    알고리즘: { index: 10, data: [23] },
+  },
+  filterType: "tag_id",
+  pathType: "category",
 };
 
-export const level: {
-  [key: string]: number;
-} = {
-  입문: 11,
-  초급: 12,
-  중급: 13,
-  고급: 14,
-  심화: 15,
+export const level: CourseFilter = {
+  filter: {
+    입문: { index: 11, data: [100] },
+    초급: { index: 12, data: [101] },
+    중급: { index: 13, data: [102] },
+    고급: { index: 14, data: [103] },
+    심화: { index: 15, data: [104] },
+  },
+  filterType: "tag_id",
+  pathType: "level",
 };
 
-export const programmingLanguage: {
-  [key: string]: number;
-} = {
-  C: 16,
-  "C++": 17,
-  자바: 18,
-  파이썬: 19,
-  자바스크립트: 20,
-  R: 21,
-  "HTML/CSS": 22,
-  SQL: 23,
-  아두이노: 24,
-  스크래치: 25,
-  코틀린: 26,
-  스위프트: 27,
-  엔트리: 28,
+export const programmingLanguage: CourseFilter = {
+  filter: {
+    C: { index: 16, data: [7] },
+    "C++": { index: 17, data: [8] },
+    자바: { index: 18, data: [9] },
+    파이썬: { index: 19, data: [10] },
+    자바스크립트: { index: 20, data: [19] },
+    R: { index: 21, data: [20] },
+    "HTML/CSS": { index: 22, data: [21] },
+    SQL: { index: 23, data: [24] },
+    아두이노: { index: 24, data: [25] },
+    스크래치: { index: 25, data: [26] },
+    코틀린: { index: 26, data: [28] },
+    스위프트: { index: 27, data: [29] },
+    엔트리: { index: 28, data: [30] },
+  },
+  filterType: "tag_id",
+  pathType: "programmingLanguage",
 };
 
-export const price: {
-  [key: string]: number;
-} = {
-  무료: 29,
-  유료: 30,
-  구독: 31,
-  학점: 32,
+export const price: CourseFilter = {
+  filter: {
+    무료: { index: 29, data: [0] },
+    유료: { index: 30, data: [0] },
+    구독: { index: 31, data: [4] },
+    학점: { index: 32, data: [6] },
+  },
+  filterType: "enroll_type",
+  pathType: "price",
 };
